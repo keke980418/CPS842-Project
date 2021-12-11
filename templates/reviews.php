57a568509fdaa22d0aa5e8415ac7122ea9553dde
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-12-10 19:39:36 -0500

    getRecommend updated.

--- templates/reviews.php
+++ templates/reviews.php
@@ -124,18 +124,10 @@ if(!isset($_SESSION))
 
   <?php
   if(isset($_GET['getRecommend'])){
+    require('../scripts/recommend.php');
     $userID = $_SESSION['userID'];
-    $sql = "SELECT * FROM reviews_table WHERE userID = $userID;";
-    $result = $dbc->query($sql);
-
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        echo $row["movie"]." ".$row["rate"]."<br>";
-      }
-    } else {
-      echo "0 results";
-    }
+    $result = recommender($userID);
+    echo "Result received";
   }
   ?>
   </body>
