4af91f920378edd223210d5ed74b5489249b330d
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-12-10 19:18:00 -0500

    adding reviews data to movie_database table

--- db/reviewProcess.php
+++ db/reviewProcess.php
@@ -3,7 +3,6 @@ require 'dbc.php';
 if(!isset($_SESSION))
 {
     session_start();
-
 }
 //If user_table doesn't exist, create table
 $tableCheck = mysqli_query($dbc,'select 1 from `reviews_table` LIMIT 1');
@@ -24,18 +23,34 @@ if(isset($_POST['submit'])){
       $rate = 0;
     }
 
+    // reviews_table
     $sql = mysqli_query($dbc, "SELECT movie FROM reviews_table WHERE movie = '$movie';");
     if (mysqli_num_rows($sql) > 0)
     {
-      //update if exist
+      // update if exist
       $sqlUpdate = "UPDATE reviews_table SET rate = '$rate' WHERE movie = '$movie';";
       mysqli_query($dbc, $sqlUpdate) or die("Error Updating user to reviews_table");
     }
     else {
-      //insert if not exist
+      // insert if not exist
       $sqlInsert="INSERT INTO reviews_table (userID, movie, rate)
           VALUES ('$userID', '$movie', '$rate');";
       mysqli_query($dbc, $sqlInsert) or die("Error Inserting user to reviews_table");
+    }
+
+    // movie_database
+    $database_sql = mysqli_query($dbc, "SELECT movieTitle, userID FROM movie_database WHERE movieTitle = '$movie' AND userID = '$userID';");
+    if (mysqli_num_rows($database_sql) > 0)
+    {
+      //update if exist
+      $sqlUpdate = "UPDATE movie_database SET rating = '$rate' WHERE movieTitle = '$movie';";
+      mysqli_query($dbc, $sqlUpdate) or die("Error Updating user to movie_database");
+    }
+    else {
+      //insert if not exist
+      $sqlInsert="INSERT INTO movie_database (userID, movieTitle, rating)
+          VALUES ('$userID', '$movie', '$rate');";
+      mysqli_query($dbc, $sqlInsert) or die("Error Inserting user to movie_database");
     }
     //Redirect To Reviews
     header("Location: ../templates/reviews.php");
